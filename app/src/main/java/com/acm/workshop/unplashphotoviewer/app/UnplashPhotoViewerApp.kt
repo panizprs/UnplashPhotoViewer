ac8d3c75bade602e09a8193620ff9d9c744e53f5
p.prs <paniz.parastar1@gmail.com>
2019-06-01 16:28:01 +0430

    initial commit

---
+++ app/src/main/java/com/acm/workshop/unplashphotoviewer/app/UnplashPhotoViewerApp.kt
@@ -0,0 +1,7 @@
+package com.acm.workshop.unplashphotoviewer.app
+
+import android.app.Application
+
+class UnplashPhotoViewerApp : Application(){
+
+}
---
+++ app/src/main/java/com/acm/workshop/unplashphotoviewer/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.acm.workshop.unplashphotoviewer.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.acm.workshop.unplashphotoviewer.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
